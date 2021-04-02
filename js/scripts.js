c39eb077e0c9b76907fc89791abc40ef79c617f9
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-02 11:30:02 -0800

    am stuck, going to take a break

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 function looperDooper(number) {
   for(let i=0; i<=number; i++) {
-    console.log(i);
-  }
+    if(i.toString().split("").includes("1")){
+      console.log("BEEP!")
+    } else {console.log(i)}
+    if(i.toString().split("").includes("2")){
+      console.log("BOOP!")
+    } else {console.log(i)}
+  } 
 };
+
+
 $(document).ready(function () {
   $("#example-form").submit(function (event) {
     event.preventDefault();
